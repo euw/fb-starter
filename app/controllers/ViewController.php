a3719688250272b2e2fbdea6e860d9dd9853fdd1
Björn Martensen <bjoern.martensen@gmail.com>
2014-09-09 15:24:14 +0200

    use empty strings if text has not been configured. really should throw an exception here instead though

--- app/controllers/ViewController.php
+++ app/controllers/ViewController.php
@@ -1,13 +1,22 @@
 <?php
 
 use Euw\FacebookApp\Modules\Texts\Repositories\TextRepository;
+use Euw\FacebookApp\Modules\Texts\Models\Text;
 
-class ViewController extends BaseController {
+class ViewController extends BaseController
+{
 
     public function __construct(TextRepository $text)
     {
         parent::__construct();
-        View::share('texts', $text->all()->first());
+
+        $texts = $text->all()->first();
+
+        if ( ! $texts) {
+            $texts = new Text;
+        }
+
+        View::share('texts', $texts);
     }
 
 } 
